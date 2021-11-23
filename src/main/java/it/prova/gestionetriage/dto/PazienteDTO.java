ac74e70c25063e27e850fcb8951c9820b106db17
JessJey <petrungaro.jessica@gmail.com>
2021-11-23 12:17:18 +0100

    Agg repository

--- src/main/java/it/prova/gestionetriage/dto/PazienteDTO.java
+++ src/main/java/it/prova/gestionetriage/dto/PazienteDTO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
